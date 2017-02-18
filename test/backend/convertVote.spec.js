944cde445b456f10cddcd323105e50b9eb0ab6b2
Christof Kälin <christof.kaelin@gmail.com>
2017-02-18 15:45:59 +0100

    starting with the test lessons now ;-)

---
+++ test/backend/convertVote.spec.js
@@ -0,0 +1,14 @@
+import expect from 'expect';
+import convertVote from '../src/common/backend/convertVote';
+import rawData from '../fixtures/rawVote';
+import expectedData from '../fixtures/convertedVote';
+
+describe('Backend', () => {
+    describe('convertVote', () => {
+        it('calculates total votes', () => {
+            const convertedVote = convertVote(rawData);
+            expect(convertedVote.totalVotes).
+                toBe(expectedData.totalVotes);
+        });
+    });
+});
