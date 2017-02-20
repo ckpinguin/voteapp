6ecc1c07315b1bfbb8e92be8787cb699e0031a35
Christof Kälin <christof.kaelin@gmail.com>
2017-02-20 14:04:50 +0100

    mocha now running sober, integration tests with karma still not, but I don't care

--- test/with_dom/VotingComponent.spec.js
+++ test/with_dom/VotingComponent.spec.js
@@ -1,10 +1,10 @@
 import expect from 'expect';
 import jsdom from 'mocha-jsdom';
 import React from 'react';
-import ReactTestUtils from 'react-addons-test-util'
-import VotingComponent from '../../../src/components/VotingComponent/VotingComponent';
-import ChoiceBar from '../../../src/common/components/ChoiceBar/ChoiceBar';
-import vote from '../../fixtures/convertedVote';
+import ReactTestUtils from 'react-addons-test-utils';
+import VotingComponent from '../../src/common/components/VotingComponent/VotingComponent';
+import ChoiceBar from '../../src/common/components/ChoiceBar/ChoiceBar';
+import vote from '../fixtures/convertedVote';
 
 describe('VotingComponent', () => {
     jsdom();
